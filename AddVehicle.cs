cf4d6cfb5b7d6d451c021980f5b168772d49a0bc
Justin-F5972 <justinfernandez27@outlook.com>
2022-01-29 19:17:48 -0800

    Made changes to design of add vehicles, and added a functioning selection for vehicle make and model

--- AddVehicle.cs
+++ AddVehicle.cs
@@ -21,5 +21,80 @@ namespace CarRepairTracker
         {
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CBMake_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RdbHonda_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Text = "Civic";
+            checkBox2.Text = "Accord";
+            checkBox3.Text = "S-2000";
+            checkBox4.Text = "CR-V";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void RdbMazda_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Text = "Mazda 3";
+            checkBox2.Text = "Mx-5 Miata";
+            checkBox3.Text = "Cx-5";
+            checkBox4.Text = "Rx-8";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void RdbNissan_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Text = "Altima";
+            checkBox2.Text = "Maxima";
+            checkBox3.Text = "Rogue";
+            checkBox4.Text = "Nissan Z";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void RdbSubaru_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Text = "Crosstrek";
+            checkBox2.Text = "Impreza";
+            checkBox3.Text = "Forester";
+            checkBox4.Text = "BRZ";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void RdbToyota_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Text = "Corolla";
+            checkBox2.Text = "Camry";
+            checkBox3.Text = "Rav-4";
+            checkBox4.Text = "Supra";
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
     }
 }
