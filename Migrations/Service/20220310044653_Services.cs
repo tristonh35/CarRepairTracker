613aa151e90484fd7a348929861709633bedbe50
Justin-F5972 <justinfernandez27@outlook.com>
2022-03-09 20:50:07 -0800

    Added table and context for services

---
+++ Migrations/Service/20220310044653_Services.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRepairTracker.Migrations.Service
+{
+    public partial class Services : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    ServiceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServiceDesription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.ServiceID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Service");
+        }
+    }
+}
