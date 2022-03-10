ece5f610aac745568f55a55d954ff2fc9eccd1eb
Justin-F5972 <justinfernandez27@outlook.com>
2022-03-09 20:35:14 -0800

    Made services class

---
+++ Services.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepairTracker
+{
+    public class Services
+    {
+        public Services() { }
+
+        [Key]
+        public int ServiceID { get; set; }
+
+        public string ServiceTitle { get; set; }
+
+        public string ServiceDesription { get; set; }   
+
+        public int Id { get; set; } 
+    }
+}
