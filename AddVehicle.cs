58cd530362c8cd1e49ce8731b4cd5bcaadf421ed
tristonh35 <tristonhall35@yahoo.com>
2022-03-09 20:30:06 -0800

    Added documentation multiple files, and did some condensing with helper methods

--- AddVehicle.cs
+++ AddVehicle.cs
@@ -19,35 +19,36 @@ namespace CarRepairTracker
             InitializeComponent();
 
         }
-
-       
-
+        /// <summary>
+        /// This is a helper method to reduce the amount of code, and sets all of the model checkboxes false by default.
+        /// </summary>
+        public void checkBoxFalse()
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+        //If the Honda button is selected, all of the models will then appear for this specific make.
         private void RdbHonda_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Text = "Civic";
             checkBox2.Text = "Accord";
             checkBox3.Text = "Odyssey";
             checkBox4.Text = "CR-V";
-
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+            checkBoxFalse();
+       
         }
-
+        //If the Mazda button is selected, all of the models will then appear for this specific make.
         private void RdbMazda_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Text = "Mazda 3";
             checkBox2.Text = "Mx-5 Miata";
             checkBox3.Text = "Cx-5";
             checkBox4.Text = "Cx-9";
-
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+            checkBoxFalse();
         }
-
+        //If the Nissan button is selected, all of the models will then appear for this specific make.
         private void RdbNissan_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Text = "Altima";
@@ -55,12 +56,9 @@ namespace CarRepairTracker
             checkBox3.Text = "Rogue";
             checkBox4.Text = "Nissan Z";
 
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+            checkBoxFalse();
         }
-
+        //If the Subaru button is selected, all of the models will then appear for this specific make.
         private void RdbSubaru_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Text = "Crosstrek";
@@ -68,12 +66,9 @@ namespace CarRepairTracker
             checkBox3.Text = "Forester";
             checkBox4.Text = "BRZ";
 
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+            checkBoxFalse();
         }
-
+        //If the Toyota button is selected, all of the models will then appear for this specific make.
         private void RdbToyota_CheckedChanged(object sender, EventArgs e)
         {
             checkBox1.Text = "Corolla";
@@ -81,10 +76,7 @@ namespace CarRepairTracker
             checkBox3.Text = "Rav-4";
             checkBox4.Text = "Supra";
 
-            checkBox1.Checked = false;
-            checkBox2.Checked = false;
-            checkBox3.Checked = false;
-            checkBox4.Checked = false;
+            checkBoxFalse();
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
@@ -127,29 +119,44 @@ namespace CarRepairTracker
             }
         }
 
-        
-            
 
 
-       
+        public void noMakeChecked()
+        {
+
+        }
+
+        /// <summary>
+        /// On the click of the submit button, validation for each field will occur before a vehicle
+        /// gets sent to the database. 
+        /// If there is anything that is invalid, then a corresponding error message will pop up for the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnSubmit_Click(object sender, EventArgs e)
         {
+            //Set the dbContext and the Vehicle object
             VehicleContext dbContext = new VehicleContext();
             Vehicle vehicle = new Vehicle();
             int length = TxtVin.Text.Length;
+            //If no vehicle make is selected, then an error message will appear for the user telling
+            // them that it is required
             if (!checkBox1.Checked && !checkBox2.Checked && !checkBox3.Checked && !checkBox4.Checked ||
                 !RdbHonda.Checked && !RdbMazda.Checked && !RdbNissan.Checked && !RdbSubaru.Checked && !RdbToyota.Checked) 
             {
                 MessageBox.Show("Please select a Make and Model");
             }
+            //If a year is not selected then the user will be prompted that it is required
             if (CBYear.Text == "")
             {
                 MessageBox.Show("Please select a year");
             }
+            //This ensures that the VIN is 17 characters long 
             if(length < 17 || length > 17)
             {
                 MessageBox.Show("Vin must be 17 characters");
             }
+            //If everything is correct, the vehicle will now be added to the database. 
             else
             {
                 if (checkBox1.Checked == true)
@@ -200,9 +207,11 @@ namespace CarRepairTracker
                     vehicle.Year = Convert.ToInt32(CBYear.Text);
                 }
                 vehicle.VIN = TxtVin.Text;
-
+                //This initializes the addition to the database
                 dbContext.Vehicles.Add(vehicle);
+                //This saves changes after a vehicle is selected
                 dbContext.SaveChanges();
+                //A success message for a vehicle being added
                 MessageBox.Show("Vehicle successfully added!");
                 
             }
--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,8 @@ namespace CarRepairTracker
             this.Close();
         }
 
+        //Upon clicking the add vehicle button, the add vehicle button will show up and allow users to add
+        // a vehicle of their choice to a database
         private void addVehicleBtn_Click(object sender, EventArgs e)
         {
             AddVehicle addVehicle = new AddVehicle();
@@ -28,7 +30,9 @@ namespace CarRepairTracker
         {
 
         }
-
+        
+        //Upon clicking the add service button, the Add Service view will show up and allow users to add
+        //a history of services to a specific vehicle
         private void addServiceBtn_Click(object sender, EventArgs e)
         {
             AddService addService = new AddService();
